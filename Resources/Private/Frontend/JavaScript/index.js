ce983186d71fff29dda304130b1e15f62d38fe4a
Björn Feischen <me@b4f7.de>
2023-03-11 23:39:11 +0100

    [BUGFIX] Add existence check for map container

--- Resources/Private/Frontend/JavaScript/index.js
+++ Resources/Private/Frontend/JavaScript/index.js
@@ -5,8 +5,12 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let map = L.map('tx_b4f7locator_map', {
-    center: [51.505, -0.09],
-    zoom: 13,
-  });
+  const mapContainer = document.querySelector('#tx-b4f7locator--map');
+
+  if (mapContainer) {
+    let map = L.map('tx-b4f7locator--map', {
+      center: [51.505, -0.09],
+      zoom: 13,
+    });
+  }
 });
